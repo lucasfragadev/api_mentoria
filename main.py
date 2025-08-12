3585c09e7136abd1a6b0d5562900d4102544883a
Gus <gustavo@Gustavos-MacBook-Air.local>
2025-08-11 21:09:01 -0300

    feat: adding endpoints for update and delete mentors

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import logging 
 from fastapi import FastAPI, status, HTTPException
 from typing import List # Vamos precisar de um Lista
 
 from schemas import Mentor, MentorCreate # Importação a partir do schemas.py
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 app = FastAPI() ## Criou uma instância do FastAPI / Criou um objeto
 
@@ -43,4 +46,40 @@ def get_mentor(mentor_id: int):
       if mentor.id == mentor_id:
          return mentor
 
-   raise HTTPException(status_code=404, detail="Mentor Not Found")
+   raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Mentor Not Found")
+
+# Enpoint: rota para ATUALIZAR os dados de um mentor no banco de dados   
+@app.put("/mentors/{mentor_id}", response_model=Mentor, status_code=status.HTTP_200_OK)
+def update_mentor(mentor_id: int, mentor_data: MentorCreate):
+   
+   for index, mentor in enumerate(db_mentors):
+      if mentor.id == mentor_id:
+         updated_mentor = Mentor(id=mentor_id, **mentor_data.model_dump())
+         db_mentors[index] = updated_mentor
+         return updated_mentor
+
+   raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Mentor Not Found")
+
+# Enpoint: rota para EXCLUIR um mentor (Exclusão física)
+@app.delete("/mentors/{mentor_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_mentor(mentor_id: int):
+   """
+   Deleta um mentor do sistema pelo Id
+   """  
+   logging.info(f"Recebida a requisicao para deletar o mentor com ID {mentor_id}")
+   
+   mentor_to_delete = None
+   for mentor in db_mentors:
+      if mentor.id == mentor_id:
+         mentor_to_delete = mentor
+         break
+   
+   if not mentor_to_delete:
+      logging.warning(f"Tentativa de deletar mentor com ID {mentor_id}, mas ele não foi encontrado")
+      raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Mentor Not Found")
+   
+   db_mentors.remove(mentor_to_delete)
+   
+   logging.info(f"Mentor com ID {mentor_id} ({mentor_to_delete.name}) foi deletado com sucesso.")
+   
+   return None
